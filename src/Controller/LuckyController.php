acfb10165e13638a847beff72026df9b4c9ae87a
Shahid <muhammadshahidraficp@gmail.com>
2019-01-22 17:19:20 +0530

    Symfony sample

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,36 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class LuckyController extends AbstractController
+{
+
+    /**
+      * @Route("/lucky/number")
+      */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+
+    }
+
+    /**
+     * @Route("/register")
+     */
+    public function register()
+    {
+        
+
+   return $this->render('registration/register.html.twig');
+
+    }
+}
+
+
---
+++ src/Entity/User.php
@@ -0,0 +1,59 @@
+<?php
+
+// src/Entity/User.php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    
+    // other properties and methods
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getname()
+    {
+        return $this->name;
+    }
+
+    public function setname($name)
+    {
+        $this->name = $name;
+    }
+
+}
